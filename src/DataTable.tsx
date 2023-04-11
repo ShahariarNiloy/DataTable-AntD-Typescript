b2e2af8a02336851d8716a053b4493b77b6c63d3
sakilk130 <sakilk130@gmail.com>
2023-04-11 15:50:07 +0600

    feat: export from api link

--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -6,6 +6,11 @@ import { MdClear } from 'react-icons/md';
 import { utils, writeFile } from 'xlsx';
 import { AxiosAuthInstance } from './api';
 
+interface IDownloadOptions {
+  url: string;
+  key?: any;
+  URL?: any;
+}
 interface PropsType {
   title?: string;
   loading?: boolean;
@@ -14,11 +19,12 @@ interface PropsType {
   query: any;
   setQuery: any;
   total?: number;
-  downloadOptions?: any;
+  downloadOptions?: IDownloadOptions;
   isPaginated?: boolean;
   isDownloadable?: boolean;
   isSearchable?: boolean;
   scroll?: any;
+  isCustomDownload?: boolean;
 }
 
 function useDebounce<T>(value: T, delay?: number): T {
@@ -48,57 +54,81 @@ export const DataTable = ({
   isDownloadable = true,
   isSearchable = true,
   scroll = { x: 1300 },
+  isCustomDownload = false,
 }: PropsType) => {
   const [search, setSearch] = useState('');
   const [downloadLoading, setDownloadLoading] = useState(false);
+  const [searchInputVisible, setSearchInputVisible] = useState(false);
+
   const colObj = columns?.map((column: any) => ({
     ...column,
     key: column?.title,
   }));
 
   const handleDownload = async () => {
-    setDownloadLoading(true);
-    AxiosAuthInstance.get(downloadOptions?.url ?? '/').then((res: any) => {
-      const apiData = res?.data?.data[Object.keys(res?.data?.data)[0]]?.data;
-
-      const sheetData = apiData?.length
-        ? apiData?.map((data: any, idx: number) => {
-            let obj: any = {
-              'Serial No.': idx + 1,
-            };
-            let [key, value]: any = [];
-            for ([key, value] of Object.entries(downloadOptions?.key)) {
-              if (typeof value === 'object') {
-                obj[key] = value?.value(data[value?.field] ?? '');
-                continue;
+    try {
+      setDownloadLoading(true);
+      if (isDownloadable && downloadOptions?.url) {
+        const res = await AxiosAuthInstance.get(downloadOptions?.url);
+        const apiData = res?.data?.data[Object.keys(res?.data?.data)[0]]?.data;
+        const sheetData = apiData?.length
+          ? apiData?.map((data: any, idx: number) => {
+              let obj: any = {
+                'Serial No.': idx + 1,
+              };
+              let [key, value]: any = [];
+              for ([key, value] of Object.entries(downloadOptions?.key)) {
+                if (typeof value === 'object') {
+                  obj[key] = value?.value(data[value?.field] ?? '');
+                  continue;
+                }
+                if (value?.includes('.')) {
+                  let nestedValue = data;
+                  value
+                    ?.split('.')
+                    ?.forEach(
+                      (nestedKey: any) =>
+                        (nestedValue = nestedValue[nestedKey] ?? '')
+                    );
+                  obj[key] = nestedValue;
+                  continue;
+                }
+                obj[key] = data[value] ?? '';
               }
+              return obj;
+            })
+          : Object.keys(downloadOptions?.key)?.map((key: string) => ({
+              [key]: '',
+            }));
 
-              if (value?.includes('.')) {
-                let nestedValue = data;
-                value
-                  ?.split('.')
-                  ?.forEach(
-                    (nestedKey: any) =>
-                      (nestedValue = nestedValue[nestedKey] ?? '')
-                  );
-                obj[key] = nestedValue;
-                continue;
-              }
-
-              obj[key] = data[value] ?? '';
-            }
-            return obj;
-          })
-        : Object.keys(downloadOptions?.key)?.map((key: string) => ({
-            [key]: '',
-          }));
-
-      const ws = utils.json_to_sheet(sheetData);
-      const wb = utils.book_new();
-      utils.book_append_sheet(wb, ws, 'Data');
-      writeFile(wb, title + '.xlsx');
+        const ws = utils.json_to_sheet(sheetData);
+        const wb = utils.book_new();
+        utils.book_append_sheet(wb, ws, 'Data');
+        writeFile(wb, title + '.xlsx');
+      }
       setDownloadLoading(false);
-    });
+    } catch (error) {
+      setDownloadLoading(false);
+      console.log(error);
+    }
+  };
+
+  const handleCustomDownload = async () => {
+    try {
+      setDownloadLoading(true);
+      if (isCustomDownload && downloadOptions?.url) {
+        const res = await AxiosAuthInstance.get(downloadOptions?.url);
+        const link = document.createElement('a');
+        link.href = downloadOptions.URL + '/' + res?.data?.data?.path;
+        link.setAttribute('download', title + '.xlsx');
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+        setDownloadLoading(false);
+      }
+    } catch (error) {
+      setDownloadLoading(false);
+    }
   };
 
   const debouncedValue = useDebounce(search, 500);
@@ -107,39 +137,39 @@ export const DataTable = ({
   }, [debouncedValue]);
 
   return (
-    <div>
-      <Table
-        loading={loading}
-        columns={colObj}
-        bordered
-        scroll={scroll}
-        dataSource={data}
-        pagination={
-          isPaginated && {
-            responsive: true,
-            showQuickJumper: true,
-            showLessItems: true,
-            defaultCurrent: query?.page ?? 1,
-            defaultPageSize: query?.take ?? 10,
-            showSizeChanger: true,
-            onChange: (page: number, pageSize: number) =>
-              setQuery({ ...query, page, take: pageSize }),
-            total,
-            showTotal: (total, range) =>
-              `${range[0]}-${range[1]} of ${total} items`,
-          }
+    <Table
+      showHeader={true}
+      loading={loading}
+      columns={colObj}
+      scroll={scroll}
+      dataSource={data}
+      pagination={
+        isPaginated && {
+          responsive: true,
+          showQuickJumper: true,
+          showLessItems: true,
+          defaultCurrent: query?.page ?? 1,
+          defaultPageSize: query?.take ?? 10,
+          showSizeChanger: true,
+          onChange: (page: number, pageSize: number) =>
+            setQuery({ ...query, page, take: pageSize }),
+          total,
+          showTotal: (total, range) =>
+            `${range[0]}-${range[1]} of ${total} items`,
         }
-        title={() =>
-          (isDownloadable || isSearchable) && (
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'end',
-                alignItems: 'center',
-                gap: '10px',
-              }}
-            >
-              {isSearchable && (
+      }
+      title={() =>
+        (isDownloadable || isSearchable) && (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'end',
+              alignItems: 'center',
+              gap: '10px',
+            }}
+          >
+            {isSearchable &&
+              (searchInputVisible ? (
                 <Input
                   bordered={false}
                   allowClear={{
@@ -147,47 +177,77 @@ export const DataTable = ({
                       <MdClear
                         title="Clear"
                         style={{ fontSize: 20, margin: 'auto' }}
+                        onClick={() => setSearchInputVisible(false)}
                       />
                     ),
                   }}
                   suffix={
                     !search && (
-                      <BiSearch
-                        title="Search"
+                      <MdClear
+                        title="Clear"
                         style={{
                           fontSize: 20,
+                          margin: 'auto',
+                          cursor: 'pointer',
+                          opacity: 0.5,
                         }}
+                        onClick={() => setSearchInputVisible(false)}
                       />
                     )
                   }
-                  style={{ borderBottom: '2px solid #ccc', width: 500 }}
+                  style={{
+                    borderBottom: '2px solid #ccc',
+                    width: 400,
+                    boxShadow: 'none',
+                  }}
                   prefix={
-                    search && (
-                      <BiSearch
-                        style={{
-                          fontSize: 20,
-                        }}
-                      />
-                    )
+                    <BiSearch
+                      style={{
+                        fontSize: 20,
+                        margin: 'auto',
+                        opacity: 0.5,
+                      }}
+                    />
                   }
                   placeholder="Search ..."
                   value={search ?? ''}
                   onChange={(e: any) => setSearch(e?.target?.value)}
                 />
-              )}
-              {isDownloadable && (
+              ) : (
                 <Button
-                  title="Download"
-                  icon={<BiDownload style={{ fontSize: 20 }} />}
-                  onClick={handleDownload}
-                  loading={downloadLoading}
+                  title="Search"
+                  icon={
+                    <BiSearch
+                      style={{
+                        fontSize: 20,
+                        margin: 'auto',
+                        display: 'flex',
+                      }}
+                    />
+                  }
+                  onClick={() => setSearchInputVisible(true)}
                 />
-              )}
-            </div>
-          )
-        }
-      />
-    </div>
+              ))}
+            {isDownloadable && (
+              <Button
+                title="Download"
+                icon={<BiDownload style={{ fontSize: 20, margin: 'auto' }} />}
+                onClick={handleDownload}
+                loading={downloadLoading}
+              />
+            )}
+            {isCustomDownload && (
+              <Button
+                title="Download"
+                icon={<BiDownload style={{ fontSize: 20, margin: 'auto' }} />}
+                onClick={handleCustomDownload}
+                loading={downloadLoading}
+              />
+            )}
+          </div>
+        )
+      }
+    />
   );
 };
 
